2fe60caaaa60e8e98d33b77e7813f8ad07bf4f7e
kanghanseo <kanghanseo2015@gmail.com>
2026-01-08 02:39:04 +0900

    Security 커스텀 및 User관련 작성

--- backend/src/main/java/com/ddip/backend/security/auth/JwtTokenFilter.java
+++ backend/src/main/java/com/ddip/backend/security/auth/JwtTokenFilter.java
@@ -28,7 +28,6 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
         String header = request.getHeader("Authorization");
 
         log.info("Authorization Header = {}", header);
