d5e5daa690ad274482e857967dea5845386eb0c9
kanghanseo <kanghanseo2015@gmail.com>
2026-01-19 15:31:46 +0900

    Repository 이름 수정

--- backend/src/main/java/com/ddip/backend/repository/ProjectRepository.java
+++ backend/src/main/java/com/ddip/backend/repository/ProjectRepository.java
@@ -1,13 +1,13 @@
 package com.ddip.backend.repository;
 
 import com.ddip.backend.entity.Project;
-import com.ddip.backend.repository.custom.ProjectCustomRepository;
+import com.ddip.backend.repository.custom.ProjectRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Long>, ProjectCustomRepository {
+public interface ProjectRepository extends JpaRepository<Project, Long>, ProjectRepositoryCustom {
     Optional<Project> findById(Long id);
 }
