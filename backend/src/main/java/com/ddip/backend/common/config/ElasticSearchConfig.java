6960d0c8755405d2dbd323f98096c3f705502ca5
kanghanseo <kanghanseo2015@gmail.com>
2026-02-27 12:39:00 +0900

    Merge remote-tracking branch 'origin/feature/modify' into dev

--- backend/src/main/java/com/ddip/backend/common/config/ElasticSearchConfig.java
+++ backend/src/main/java/com/ddip/backend/common/config/ElasticSearchConfig.java
@@ -3,6 +3,10 @@ package com.ddip.backend.common.config;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.jetbrains.annotations.NotNull;
@@ -33,9 +37,14 @@ public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
     @Bean
     public ElasticsearchClient elasticsearchClient() {
+        ObjectMapper objectMapper =
+                new ObjectMapper()
+                        .registerModule(new JavaTimeModule())
+                        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
         RestClient restClient = RestClient.builder(HttpHost.create(uri)).build();
 
-        RestClientTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+        RestClientTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper(objectMapper));
 
         return new ElasticsearchClient(transport);
     }
