4a8e71e48b06adde1494e253734f9d43133b1164
YeonWoo <kobin4215@naver.com>
2026-04-30 16:02:43 +0900

    fix: createIndex=true

--- backend/src/main/java/com/ddip/backend/auction/es/document/AuctionDocument.java
+++ backend/src/main/java/com/ddip/backend/auction/es/document/AuctionDocument.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Document(indexName = "auction", createIndex = false, writeTypeHint = WriteTypeHint.FALSE)
+@Document(indexName = "auction", createIndex = true, writeTypeHint = WriteTypeHint.FALSE)
 @Setting(settingPath = "elasticsearch/setting.json")
 @Mapping(mappingPath = "elasticsearch/auction-mapping.json")
 public class AuctionDocument {
